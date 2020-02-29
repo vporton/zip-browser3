e482fee0ae8bc6cfcb9e27c83176066c1eaaea9b
Victor Porton <porton@narod.ru>
2020-02-29 15:03:06 +0200

    rm obj/

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace zip_browser3
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            //services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,11 +41,11 @@ namespace zip_browser3
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            //app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
